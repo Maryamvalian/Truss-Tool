63e95840cbab90f12428c44b757a3a12e2fb983b
maryamvalian <maryam.valian@gmail.com>
2023-02-26 18:24:27 -0500

    updated calculate react module

--- src/CalculateReact.py
+++ src/CalculateReact.py
@@ -1,10 +1,33 @@
 #      Calculate Support Reactions
 from sympy import *
+#test case (temporary)
+n=3
+J=[[0, 0],[1, 1],[2, 0],[3,1],[4,0],[5,1]]
+M=[[1, 2],[1, 3],[2 ,3],[2,4],[3,4],[3,5],[4,5],[4,6],[5,6]]
+F=[[0 ,0],[0, -5],[0 ,0],[-3,-2],[0,0],[0,0]]
+p=0   # location(0,0)
+r=2   #location(2,0)
+#Support Reaction 
+# Assumption: Only One pinned support p , One roller support r
+# create first Equation- Sum(Fx)=0
+sum=0
+for i in range(len(F)):
+    sum = F[i][0] + sum
+PX=-sum 
+print("\n Reactions Calculated Successfully. \n PX:",PX)
 
-
-x,y,z= symbols('x y z')
-e1=Eq(2*x-y-z,0)
-e2=Eq(-4*x+2*y-5*z,6)
-e3=Eq(x+y-20*z,5)
-sol=solve((e1,e2,e3),(x,y,z))
+#create Equation  : SUM(FY)=0
+#PY Pinned support Y-reaction, RY poller support Y-reaction   
+sum=0
+for i in range(len(F)):
+    sum = F[i][1] + sum 
+PY,RY= symbols('PY RY')
+e1=Eq(PY+RY,-sum)
+#create eq Sum(M)=0 pivot :(0,0)
+sum=0
+for i in range(len(F)):
+    sum = (F[i][1])*J[i][0] + sum       #Perpendicular F, distance to (0,0)
+e2=Eq(PY*J[p][0]+RY*J[r][0],-sum)      
+#Solve All equations
+sol=solve((e1,e2),(PY,RY))
 print(sol)
